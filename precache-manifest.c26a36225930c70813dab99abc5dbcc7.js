eff984cdcfdddd99894380246adb2ddb7d99fee5
William Wen <wenwi@utschools.ca>
2019-06-26 10:59:54 +0800

    Updates

---
+++ precache-manifest.c26a36225930c70813dab99abc5dbcc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a92f2655617a8703d90775f94cf7de",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e2d1f78392da422eddb8",
+    "url": "/react-calculator/static/css/main.eb6c7cb6.chunk.css"
+  },
+  {
+    "revision": "e1f4beb362db09a7135f",
+    "url": "/react-calculator/static/js/2.ad6e43c5.chunk.js"
+  },
+  {
+    "revision": "e2d1f78392da422eddb8",
+    "url": "/react-calculator/static/js/main.6dc38af6.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
